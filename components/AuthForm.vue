0ae4b41163a8167918b01e0ed3da2814ce6006d9
WilliamBostrom <133894488+WilliamBostrom@users.noreply.github.com>
2024-11-25 15:41:45 +0100

    basic login/register page

--- components/AuthForm.vue
+++ components/AuthForm.vue
@@ -3,17 +3,44 @@
     <div class="flex flex-row">
       <div class="bg-gray-50 mr-7">
         <form
-          class="flex flex-col items-start border-r border-gray-300 w-full form-css"
+          class="card flex flex-col items-start justify-center border-r border-gray-300 w-full form-css p-3"
           action=""
         >
-          <input type="text" placeholder="Name" name="name" />
-          <input type="text" placeholder="Email" name="email" />
-          <input type="password" placeholder="Password" name="password" />
-          <input
-            type="password"
-            placeholder="Confirm password"
-            name="password"
-          />
+          <label for="name"
+            >Username
+            <input
+              v-if="isRegistration"
+              type="text"
+              placeholder="Your name"
+              name="name"
+          /></label>
+          <label for="email"
+            >Email
+            <input type="text" placeholder="example@mail.com" name="email"
+          /></label>
+          <label for="password"
+            >Password
+            <input type="password" placeholder="Password" name="password" />
+          </label>
+          <label for="password"
+            >Confirm password
+            <input
+              v-if="isRegistration"
+              type="password"
+              placeholder="Confirm password"
+              name="password"
+          /></label>
+          <button class="btn w-[250px]">
+            {{ isRegistration ? "Sign up" : "Submit" }}
+          </button>
+          <p class="mt-3" v-if="isRegistration">
+            Already have an account?
+            <a class="underline" href="/login">Login</a>
+          </p>
+          <p class="mt-3" v-if="!isRegistration">
+            Do not have an account yet?
+            <a class="underline" href="/register">Register</a>
+          </p>
         </form>
       </div>
       <div>
@@ -38,16 +65,25 @@ export default defineComponent({
 
 <style scoped>
 form {
-  padding: 10px;
+  padding: 20px;
+  height: 400px;
+  padding-top: 20px;
 }
 
 .form-css input {
   width: 250px;
+  border-radius: 5px;
   margin-bottom: 16px;
   padding: 6px;
+  border: 1px solid black;
 }
 
 .form-css input:last-of-type {
-  margin-bottom: 30px;
+  margin-bottom: 15px;
+}
+
+label {
+  display: flex;
+  flex-direction: column;
 }
 </style>
